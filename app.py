6b85054324e711955852f694457c43a57a4ae368
kianoush <kianoushit1999@gmail.com>
2022-05-18 07:38:13 -0700

    completed base of fetch preparation of sutable data (#3)

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def show_index():
     return render_template("index.html")
+
+@app.route('/start-engine')
+def prepare_suitable_data():
+    print("entered in start engine")
+    tic = time.time()
+    time.sleep(5)
+    toc = time.time()
+    return jsonify({
+        "time": toc - tic,
+        "response": "success"
+    })
 
 @app.route('/fetch-related-data', methods=["GET"])
 def fetch_data():
--- static/index.js
+++ static/index.js
@@ -6,23 +6,41 @@ const btnDelete = document.querySelector('#btn-delete');
 const gearBtn = document.querySelector('.gear-btn');
 const gearBtnIcon = document.querySelectorAll('.gear-btn-icon');
 const hourglassIcon = document.querySelector('#hourglass');
+const engineTime = document.querySelector('#engine-time');
 
-btnSelect.addEventListener('click', ()=>{
+btnSelect.addEventListener('click', () => {
     console.log('click');
     fetch('http://127.0.0.1:5000/fetch-related-data')
-    .then(response => response.json())
-    .then(data => console.log(data));
+        .then(response => response.json())
+        .then(data => console.log(data));
 })
 
-btnDelete.addEventListener('click', ()=>{
+btnDelete.addEventListener('click', () => {
     console.log('delete');
     inuptSearch.value = "";
 })
 
-gearBtn.addEventListener('click', ()=>{
+gearBtn.addEventListener('click', () => {
+    const tic = new Date().getTime()
+    const interval = setInterval(() => {
+        let toc = new Date().getTime();
+        engineTime.textContent = `${parseFloat((toc - tic) / 1000).toFixed(0)}`
+    }, 1000);
     console.log('gear');
     for (const obj of gearBtnIcon) {
-        obj.classList.toggle("gear-animation")
+        obj.classList.add("gear-animation");
     }
-    hourglassIcon.classList.toggle("hourglass-animation")
+    hourglassIcon.classList.add("hourglass-animation");
+    fetch("http://127.0.0.1:5000/start-engine")
+        .then(response => response.json())
+        .then((data) => {
+            const {response} = data;
+            if (response == "success") {
+                for (const obj of gearBtnIcon) {
+                    obj.classList.remove("gear-animation");
+                }
+                hourglassIcon.classList.remove("hourglass-animation");
+                clearInterval(interval);
+            }
+        });
 })
