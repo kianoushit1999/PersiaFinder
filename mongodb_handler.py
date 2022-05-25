5d0127d3c9847b460e968e234c086110846bffe3
kianoush <kianoushit1999@gmail.com>
2022-05-25 12:08:53 -0700

    handlers for fetch and insert data into local mongodb database (#6)

---
+++ mongodb_handler.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pymongo
+
+
+def insert_general_into_db(N=0, total_vocab_size=0, total_vocab=[], DF={}):
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col = db["generalInfo"]
+    col.insert_one({
+        "N": N,
+        "total_vocab_size": total_vocab_size,
+        "total_vocab": total_vocab,
+        "DF": DF
+    })
+
+
+def fetch_general_info():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col = db["generalInfo"]
+    data = list(col.find({}, {}))[0]
+    return (data['N'],
+            data['total_vocab_size'],
+            data['total_vocab'],
+            data['DF'])
+
+
+def insert_tfidf(doc_id, word, tfidf):
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col_tfidf = db["tfidfInfo"]
+
+    col_tfidf.insert_one({
+        "doc_id": doc_id,
+        "word_id": word,
+        "tfidf": tfidf
+    })
+
+
+def find_by_doc(doc_id):
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col_tfidf = db["tfidfInfo"]
+
+    return list(map(lambda x: (x["word_id"], x["tfidf"]), np.array(list(col_tfidf.find({"doc_id": doc_id}, {
+        "word_id": 1, "tfidf": 1, "_id": 0
+    }).sort("word_id")))))
+
+
+def insert_dataframe(doc_number, doc_id, doc_url, doc_title, doc_body):
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col_df = db["dataFrame"]
+    col_df.insert_one({
+        "doc_number": doc_number,
+        "doc_id": doc_id,
+        "doc_url": doc_url,
+        "doc_title": doc_title,
+        "doc_body": doc_body
+    })
+
+
+def fetch_dataframe_by_doc_id(doc_num):
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["searchEngine"]
+    col_df = db["dataFrame"]
+    return col_df.find_one({"doc_number": doc_num}, {
+        "doc_url": 1,
+        "doc_title": 1,
+        "doc_body": 1,
+        "_id": 0
+    }).values()
