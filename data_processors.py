a5ec8b1734361dd80ddfbac78e57642e1ed6c8fe
kianoush <kianoushit1999@gmail.com>
2022-05-25 12:09:57 -0700

    preprocess entire data and save it into db

--- data_processors.py
+++ data_processors.py
@@ -1,27 +1,39 @@
 import re
-import math
 import xml.etree.ElementTree as ET
-import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
-from num2fawords import words
 from collections import Counter
 from hazm import *
 
 from handler import load_persian_stopwords, \
     get_all_files_path, remove_punctuation, convert_numbers
+from mongodb_handler import insert_general_into_db, insert_tfidf, insert_dataframe, fetch_general_info
 
 PERSIAN_STOPWORDS = load_persian_stopwords()
 
+
+def doc_freq(word, DF):
+    c = 0
+    try:
+        c = DF[word]
+    except:
+        pass
+    return c
+
+
 def fetch_data_per_path():
-    #define variables
+    # define variables
     processed_text = []
     processed_title = []
-    dataframe = []
     document_number = 0
 
+    word_counter = 0
     for path in get_all_files_path():
         print(path)
+        if (path.startswith("./source_file/Tebyan")):
+            continue
+        if (word_counter == 2):
+            break
         root = ET.parse(path).getroot()
         for doc_id, url_tag, html_body in zip(root.findall('DOC/DOCID'), root.findall('DOC/URL'),
                                               root.findall('DOC/HTML')):
@@ -31,10 +43,10 @@ def fetch_data_per_path():
             document_url = url_tag.text
 
             bs = BeautifulSoup(html_body.text, 'html5lib')
-            title: str = bs.title.get_text() if(bs.title) is not None else ""
+            title: str = bs.title.get_text() if (bs.title) is not None else ""
 
             stemmer = Stemmer()
-            if(title != ""):
+            if (title != ""):
                 title = re.sub(r'\<\!\-\-.*\-\-\>', ' ', title)
                 title = re.sub(r'[a-zA-Z]', '', title)
                 title = remove_punctuation(title)
@@ -69,76 +81,22 @@ def fetch_data_per_path():
 
             processed_text.append(body_words.split())
             processed_title.append(title_words.split())
-            dataframe.append([document_id, document_url, title, body_content])
+
+            insert_dataframe(doc_number=document_number - 1, doc_id=document_id, doc_url=document_url, doc_title=title,
+                             doc_body=body_content)
+        word_counter += 1
 
     return (
         document_number,
         processed_title,
-        processed_text,
-        dataframe
+        processed_text
     )
 
 
-def doc_freq(word):
-    c = 0
-    try:
-        c = DF[word]
-    except:
-        pass
-    return c
-
-# TF-IDF Cosine Similarity Ranking
-def cosine_sim(a, b):
-    cos_sim = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-    return cos_sim
-
-def gen_vector(tokens):
-    Q = np.zeros((len(total_vocab)))
-    counter = Counter(tokens)
-    words_count = len(tokens)
-    for token in np.unique(tokens):
-        tf = counter[token] / words_count
-        df = doc_freq(token)
-        idf = math.log((N + 1) / (df + 1))
-        try:
-            ind = total_vocab.index(token)
-            Q[ind] = tf * idf
-        except:
-            pass
-    return Q
-
-
-def cosine_similarity(query):
-    print("Cosine Similarity")
-    query = re.sub(r'[a-zA-Z]', '', query)
-    query = remove_punctuation(query)
-    query = convert_numbers(query)
-    preprocessed_query = re.sub(r'\s+', ' ', query)
-
-    stemmer = Stemmer()
-    tokenized_words_query = word_tokenize(preprocessed_query)
-    tokens = [stemmer.stem(word) for word in tokenized_words_query if not word in set(PERSIAN_STOPWORDS)]
-
-    print("\nQuery:", query)
-    print("")
-    print(tokens)
-
-    d_cosines = []
-
-    query_vector = gen_vector(tokens)
-
-    for d in D:
-        d_cosines.append(cosine_sim(query_vector, d))
-
-    out = np.array(d_cosines).argsort()[:][::-1]
-
-    return out
-
 if __name__ == '__main__':
 
-    document_number, processed_title, processed_text, dataframe = fetch_data_per_path()
-    print("Enterd main .........")
-    dataframe = pd.DataFrame(columns=("doc_id", "doc_url", "title", "body"), data=dataframe)
+    document_number, processed_title, processed_text = fetch_data_per_path()
+    print("Entered main .........")
 
     N = document_number
     DF = {}
@@ -163,6 +121,8 @@ if __name__ == '__main__':
     total_vocab_size = len(DF)
     total_vocab = [x for x in DF]
 
+    insert_general_into_db(N, total_vocab_size, total_vocab, DF)
+
     # TFIDF for body
     doc = 0
     tf_idf = {}
@@ -175,15 +135,14 @@ if __name__ == '__main__':
 
         for token in np.unique(tokens):
             tf = counter[token] / words_count
-            df = doc_freq(token)
+            df = doc_freq(token, DF)
             idf = np.log((N + 1) / (df + 1))
             tf_idf[doc, token] = tf * idf
-
         doc += 1
 
+    print("start tfidf")
     # TFIDF for title
     doc = 0
-
     tf_idf_title = {}
 
     for i in range(N):
@@ -194,13 +153,13 @@ if __name__ == '__main__':
 
         for token in np.unique(tokens):
             tf = counter[token] / words_count
-            df = doc_freq(token)
+            df = doc_freq(token, DF)
             idf = np.log((N + 1) / (df + 1))  # numerator is added 1 to avoid negative values
             tf_idf_title[doc, token] = tf * idf
         doc += 1
 
-    print(tf_idf)
     # Merging the TF-IDF according to weights
+    print("start alpha")
     alpha = 0.3
     for i in tf_idf:
         tf_idf[i] *= alpha
@@ -208,19 +167,14 @@ if __name__ == '__main__':
     for i in tf_idf_title:
         tf_idf[i] = tf_idf_title[i]
 
-    print(len(tf_idf))
-
-    D = np.zeros((N, total_vocab_size), dtype='uint8')
     for i in tf_idf:
-        try:
-            ind = total_vocab.index(i[1])
-            D[i[0]][ind] = tf_idf[i]
-        except:
-            pass
+        ind = total_vocab.index(i[1])
+        insert_tfidf(doc_id=i[0], word=ind, tfidf=tf_idf[i])
 
-    print("Ended vectorizing")
-    Q = cosine_similarity("به سايت پرديس ابوريحان دانشگاه تهران")
-    print(Q)
-    print(dataframe.loc[Q[0], :].title)
-    print(dataframe.loc[Q[1], :].title)
-    print(dataframe.loc[Q[2], :].title)
+    fetch_general_info()
+    # print("Ended vectorizing")
+    # Q = cosine_similarity("به سايت پرديس ابوريحان دانشگاه تهران")
+    # print(Q)
+    # print(dataframe.loc[Q[0], :].title)
+    # print(dataframe.loc[Q[1], :].title)
+    # print(dataframe.loc[Q[2], :].title)
